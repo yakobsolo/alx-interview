5b16b8c4eb2d533eeae530889b7336d6c056eb63
yakobsolo <yakisolo2009@gmail.com>
2022-12-20 09:27:17 +0000

    pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -13,15 +13,14 @@ def pascal_triangle(n):
         list of lists
     """
 
-    if n<=0:
+    if n <= 0:
         return []
 
     pascalTriangle = [[1]]
-    
     for i in range(n-1):
         temp = [0]+pascalTriangle[-1]+[0]
         arr = []
-        for j in range(len(temp) -1):
+        for j in range(len(temp) - 1):
             arr.append(temp[j]+temp[j+1])
         pascalTriangle.append(arr)
     return pascalTriangle

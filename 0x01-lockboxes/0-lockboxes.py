d852b5e07b727a2ec3e2d53e1dc93cc5d7c91587
yakobsolo <yakisolo2009@gmail.com>
2023-01-04 13:11:41 +0000

    readme and lock boxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,4 +1,14 @@
+#!/usr/bin/python3
+"""
+Return True if all boxes can be opened, else return False
+"""
+
+
 def canUnlockAll(boxes):
+    " 
+    T.C O(n) 
+    S.C O(n)
+    "
     keys = []
     for box in boxes:
         keys+=box
